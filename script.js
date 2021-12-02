37ce479504d8629330febdd1c68d92a1d3e9bcc4
Sheila Kerber <sheila.skerber@gmail.com>
2021-12-02 15:39:00 -0300

    js logic to create the html transaction

--- script.js
+++ script.js
@@ -10,3 +10,46 @@ const Modal = {
         alert("close")
     }
 }
+
+// array para guardar as transactions
+const transactions = [{
+        id: 1,
+        description: 'Luz',
+        amount: -15000,
+        date: '01/01/2021'
+    },
+    {
+        id: 2,
+        description: 'Aluguel',
+        amount: -120000,
+        date: '01/01/2021'
+    },{
+        id: 3,
+        description: 'Itens domésticos',
+        amount: 8000,
+        date: '09/01/2021'
+    }
+]
+
+const DOM = {
+    addTransaction(transaction, index){
+        console.log(transaction)
+        const tr = document.createElement('tr')
+        tr.innerHTML = this.innerHtmlTransaction(transaction)
+
+        console.log(tr.innerHTML)
+    },
+    innerHtmlTransaction(transaction){
+        const html = `
+                <td class="description">${transaction.description}</td>
+                <td class="expense">${transaction.amount}</td>
+                <td class="date">${transaction.date}</td>
+                <td>
+                    <img src="./assets/minus.svg" alt="Remover transação">
+                </td>
+        `
+        return html
+    }
+}
+
+DOM.addTransaction(transactions[2])
