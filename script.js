d91277a0c1047a524d375addd9c36242fc7d5e9b
Sheila Kerber <sheila.skerber@gmail.com>
2021-12-02 16:35:05 -0300

    sets amount to brazilian currency

--- script.js
+++ script.js
@@ -29,6 +29,23 @@ const transactions = [{
     }
 ]
 
+
+// funcionalidades úteis diversas
+// obj com funcionalidade para formatar o valor para a moeda brasileira
+const Utils = {
+    formatCurrency(value){
+        const signal = Number(value) < 0 ? "-" : "" 
+
+        value = String(value).replace(/\D/g, "")
+        value = Number(value) / 100
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+        return signal + value
+    }
+}
+
 // obj com as funcionalidades para criação do hmtl de cada nova transação inserida pelo usuário
 const DOM = {
     transactionsContainer: document.querySelector('#dataTable tbody'),
@@ -41,9 +58,12 @@ const DOM = {
     },
     innerHtmlTransaction(transaction){
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
+        
+        const amount = Utils.formatCurrency(transaction.amount)
+
         const html = `
                 <td class="description">${transaction.description}</td>
-                <td class= "${CSSclass}">${transaction.amount}</td>
+                <td class= "${CSSclass}">${amount}</td>
                 <td class="date">${transaction.date}</td>
                 <td>
                     <img src="./assets/minus.svg" alt="Remover transação">
